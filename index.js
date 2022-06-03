edb8550b693d013b56eb4136767bf7b7dc4ea900
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-06-03 14:11:09 +0300

    commit tree c6ad605a3e1d43be386e1d6ba8b2fe386c0a07d9

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a,b) {
+    return a+b;
+}
+function subtract(a,b){
+
+    return a-b;
+}
+
+function multiply(a,b) {
+
+    return a*b;
+}
+
+function divide(a,b) {
+
+    return a/b;
+}
+function increment(n) {
+    
+    ++n
+
+    return n;
+}
+
+function decrement(n) {
+  
+    --n
+
+    return n;
+}
+
+function makeInt(n) {
+
+    let num1 = parseInt(n, 10);
+    return num1;
+}
+
+function preserveDecimal(n) {
+
+    let num1 = parseFloat(n, 10);
+    return num1;
+
+}
